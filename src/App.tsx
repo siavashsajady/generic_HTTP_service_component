e49efd7ee09c9b81d410f8687037fb6f815d6274
Siavash Sajady <hubgitpro@gmail.com>
2023-03-24 12:57:23 +0000

    Refactor App component using  useUsers custom hook implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import useUsers from './hooks/useUsers';
 import { AxiosError, CanceledError } from './services/api-client';
 import userServices, { User } from './services/user-services';
 
 const App = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    const { request, cancel } = userServices.getAll<User>();
-    request
-      .then((res) => {
-        setUsers(res.data);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError((err as AxiosError).message);
-        setIsLoading(false);
-      });
-
-    return () => cancel();
-  }, []);
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { AxiosError, CanceledError } from '../services/api-client';
+import userServices, { User } from '../services/user-services';
+
+const useUsers = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const { request, cancel } = userServices.getAll<User>();
+    request
+      .then((res) => {
+        setUsers(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError((err as AxiosError).message);
+        setIsLoading(false);
+      });
+
+    return () => cancel();
+  }, []);
+
+  return { users, error, isLoading, setUsers, setError };
+};
+
+export default useUsers;
